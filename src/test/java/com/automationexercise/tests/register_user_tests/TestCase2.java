9cdff31852787ba0a7230a3b25c376541c3eef1b
Skirta_PC <skirtass@gmail.com>
2026-01-23 14:10:19 +0100

    feat: updating TestCase1.java and Started TestCase2.java (vol2)

---
+++ src/test/java/com/automationexercise/tests/register_user_tests/TestCase2.java
@@ -0,0 +1,25 @@
+package com.automationexercise.tests.register_user_tests;
+
+import com.automationexercise.pages.HomePage;
+import com.automationexercise.pages.LoginPage;
+import com.automationexercise.tests.BaseTest;
+import org.testng.annotations.Test;
+
+public class TestCase2 extends BaseTest {
+
+    @Test
+    public void testCase2() {
+        new HomePage(driver)
+                .openHomePage()
+                .clickConsentButton()
+                .assertHomePageIsSuccessfullyLoaded();
+
+        mainMenu.clickSignupLoginButton();
+
+        new LoginPage(driver)
+                .assertLoginToYourAccountTextIsVisible()
+
+
+
+    }
+}
