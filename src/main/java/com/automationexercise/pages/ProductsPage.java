a6231cd3dd37c06cb5c9c6a6431ffbde858b973d
Viacheslav <skirtass@gmail.com>
2026-02-24 17:53:12 +0100

    refactoring method chooseRandomProductAndClickViewButton() and add 2 new methods getAllProductsNames() + getAllProductsNamesAndPrices()

--- src/main/java/com/automationexercise/pages/ProductsPage.java
+++ src/main/java/com/automationexercise/pages/ProductsPage.java
@@ -5,10 +5,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class ProductsPage extends BasePage {
+
+    public ProductsPage(WebDriver driver, ProductModel choosenProductName, WebElement randomProduct) {
+        super(driver);
+    }
 
     public ProductsPage(WebDriver driver) {
         super(driver);
@@ -34,20 +39,46 @@ public class ProductsPage extends BasePage {
     }
 
     public ProductDetailsPage chooseRandomProductAndClickViewButton() {
-        removeAds();
-        List<WebElement> products = driver.findElements(productContainerLocator);
-        int randomProductNumber = new Random().nextInt(products.size());
-        WebElement randomProduct = products.get(randomProductNumber);
-        String name = randomProduct.findElement(relativeNameLocator).getText();
-        String price = randomProduct.findElement(relativePriceLocator).getText();
-        ProductModel choosenProduct = ProductModel.builder()
-                .name(name)
-                .price(price)
-                .build();
-        WebElement viewButton = randomProduct.findElement(relativeViewButtonLocator);
+        List<ProductModel> allProductsNamesAndPrices = getAllProductsNamesAndPrices();
+        List<WebElement> productContainer = driver.findElements(productContainerLocator);
+        int randomProductNumber = new Random().nextInt(allProductsNamesAndPrices.size());
+        ProductModel choosenProduct = allProductsNamesAndPrices.get(randomProductNumber);
+        WebElement targetConatainer = productContainer.get(randomProductNumber);
 
-        String url = viewButton.getAttribute("href");
+        String url = targetConatainer.findElement(relativeViewButtonLocator).getAttribute("href");
         driver.get(url);
         return new ProductDetailsPage(driver, choosenProduct);
+    }
+
+    public List<ProductModel> getAllProductsNames() {
+        removeAds();
+        waitUntilVisibilityOfElementLocated(productContainerLocator);
+        List<WebElement> productContainer = driver.findElements(productContainerLocator);
+        List<ProductModel> productList = new ArrayList<>();
+        for (WebElement container : productContainer){
+            String name = container.findElement(relativeNameLocator).getText();
+            ProductModel names = ProductModel.builder()
+                    .name(name)
+                    .build();
+            productList.add(names);
+        }
+        return productList;
+    }
+
+    public List<ProductModel> getAllProductsNamesAndPrices() {
+        removeAds();
+        waitUntilVisibilityOfElementLocated(productContainerLocator);
+        List<WebElement> productContainer = driver.findElements(productContainerLocator);
+        List<ProductModel> productList = new ArrayList<>();
+        for (WebElement container : productContainer){
+            String name = container.findElement(relativeNameLocator).getText();
+            String price = container.findElement(relativePriceLocator).getText();
+            ProductModel names = ProductModel.builder()
+                    .name(name)
+                    .price(price)
+                    .build();
+            productList.add(names);
+        }
+        return productList;
     }
 }
